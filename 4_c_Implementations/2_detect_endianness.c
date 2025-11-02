9bd9775c6fb7ebe15cb606c4c803d317a5ab486b
Yash Deshpande <23yashd@gmail.com>
2025-11-02 07:39:59 -0600

    Updated comments alignment in detect endiannaess code

--- 4_c_Implementations/2_detect_endianness.c
+++ 4_c_Implementations/2_detect_endianness.c
@@ -29,12 +29,23 @@ of a multi-byte data type like an int.
 
 
 Explanation:
-An unsigned int variable test_value is initialized to 1. In memory, this would be represented differently depending on the endianness:
-Little Endian: The least significant byte (LSB) is stored at the lowest memory address. So, for a 4-byte integer, it would be 01 00 00 00 (hex).
-Big Endian: The most significant byte (MSB) is stored at the lowest memory address. So, for a 4-byte integer, it would be 00 00 00 01 (hex).
-A char pointer byte_pointer is created and made to point to the memory location of test_value. Since char is typically 1 byte, dereferencing byte_pointer will access the first byte of test_value.
-If the value at the first byte (*byte_pointer) is 1, it indicates that the LSB is at the lowest memory address, meaning the system is Little Endian.
-If the value at the first byte is 0, it indicates that the MSB is at the lowest memory address (and the LSB is at a higher address), meaning the system is Big Endian. 
+An unsigned int variable test_value is initialized to 1. In memory, this would be 
+represented differently depending on the endianness:
+
+Little Endian: The least significant byte (LSB) is stored at the lowest memory address. 
+So, for a 4-byte integer, it would be 01 00 00 00 (hex).
+
+Big Endian: The most significant byte (MSB) is stored at the lowest memory address. 
+So, for a 4-byte integer, it would be 00 00 00 01 (hex).
+
+A char pointer byte_pointer is created and made to point to the memory location of test_value. 
+Since char is typically 1 byte, dereferencing byte_pointer will access the first byte of test_value.
+
+If the value at the first byte (*byte_pointer) is 1, it indicates that the LSB is at the 
+lowest memory address, meaning the system is Little Endian.
+
+If the value at the first byte is 0, it indicates that the MSB is at the lowest memory address 
+(and the LSB is at a higher address), meaning the system is Big Endian. 
 
 
 */
