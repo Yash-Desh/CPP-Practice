61dc0483b9068cbb834f232099a14ce2124a9dde
Yash Deshpande <23yashd@gmail.com>
2026-04-19 13:22:51 -0700

    Added files for topological sort in Graph, DFS & BFS method

---
+++ 2_cpp_DSA/15_Graphs/8_topological_sort_DFS.cpp
@@ -0,0 +1,59 @@
+// Author: Yash Deshpande
+// Date  : 19-04-2026
+// Tutor : Love Babbar (https://youtu.be/T_boOrr0rvk?si=3zxHv7s7yxFVeBqF)
+// Link to Question: https://www.naukri.com/code360/problems/topological-sort_982938
+
+#include<iostream>
+#include<unordered_map>
+#include<vector>
+#include<list>
+#include<stack>
+using namespace std;
+
+void createAdjList(int e, vector<vector<int>> &edges, unordered_map<int, list<int>> &adjList){
+    for(int i=0; i<e; i++){
+        int u = edges[i][0];
+        int v = edges[i][1];
+
+        // Directed Graph
+        adjList[u].push_back(v);
+    }
+}
+
+void solve(int node, unordered_map<int, list<int>> &adjList, unordered_map<int, bool> &visited, stack<int> &st) {
+    visited[node] = true;
+    for(auto neighbor : adjList[node]) {
+        if(!visited[neighbor]) {
+            solve(neighbor, adjList, visited, st);
+        }
+    }
+    st.push(node);
+}
+
+vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
+    // Write your code here
+    // Create Adjacency List
+    unordered_map<int, list<int>> adjList;
+    createAdjList(e, edges, adjList);
+
+    unordered_map<int, bool> visited;
+    stack<int> st;
+
+    // Solve for all disconnected components. 
+    for(int i=0; i<v; i++) {
+        if(!visited[i]) {
+            solve(i, adjList, visited, st);
+        }
+    }
+    vector<int> ans;
+    while(!st.empty()) {
+        ans.push_back(st.top());
+        st.pop();
+    }
+    return ans;
+}
+
+int main() {
+
+    return 0;
+}
---
+++ 2_cpp_DSA/15_Graphs/9_topological_sort_BS_Kahn_Algo.cpp
@@ -0,0 +1,76 @@
+// Author: Yash Deshpande
+// Date  : 19-04-2026
+// Tutor : Love Babbar (https://youtu.be/6XmzL04mlgQ?si=2dbYNKgivx5KXItI)
+// Link to Question: https://www.naukri.com/code360/problems/topological-sort_982938
+
+
+#include<iostream>
+#include<unordered_map>
+#include<list>
+#include<queue>
+using namespace std;
+
+void createAdjList(int e, vector<vector<int>> &edges, unordered_map<int, list<int>> &adjList) {
+    for(int i=0; i<e; i++) {
+        int u = edges[i][0];
+        int v = edges[i][1];
+
+        // Directed Graph
+        adjList[u].push_back(v);
+    }
+}
+
+void getIndegree(int v, unordered_map<int, list<int>> &adjList, vector<int> &inDegree) {
+    for(auto i: adjList) {
+        for(auto j: i.second) {
+            inDegree[j]++;
+        }
+    }
+}
+
+void topoSort(int node, unordered_map<int, list<int>> &adjList) {
+    
+}
+
+vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
+    // Write your code here
+    // Create Adjacency List
+    unordered_map<int, list<int>> adjList;
+    createAdjList(e, edges, adjList);
+
+
+    // Determine indegree of each node. 
+    vector<int> inDegree(v);
+    getIndegree(v, adjList, inDegree);
+
+    // Vector to store ans
+    vector<int> ans;
+
+    // Put all elements with 0-indegree into the queue.
+    queue<int> q;
+    for(int i=0; i<v; i++) {
+        if(inDegree[i] == 0) {
+            q.push(i);
+        }
+    }
+
+    while(!q.empty()) {
+        auto front = q.front();
+        q.pop();
+        ans.push_back(front);
+
+        for(auto neighbor : adjList[front]) {
+            inDegree[neighbor]--;
+            if(inDegree[neighbor] == 0) {
+                q.push(neighbor);
+            }
+        }
+    }
+    
+    return ans;
+}
+
+int main() {
+
+    return 0;
+}
