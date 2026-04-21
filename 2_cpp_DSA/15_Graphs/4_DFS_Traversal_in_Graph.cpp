4410f52825174cfffd35d487589903d5a58edd27
Yash Deshpande <23yashd@gmail.com>
2026-04-20 21:29:00 -0700

    Added function to perform DFS in a Graph

--- 2_cpp_DSA/15_Graphs/4_DFS_Traversal_in_Graph.cpp
+++ 2_cpp_DSA/15_Graphs/4_DFS_Traversal_in_Graph.cpp
@@ -11,6 +11,52 @@ Given an undirected and disconnected graph G(V, E), containing 'V' vertices and 
 
 */
 
+
+void createAdjList(vector<vector<int>> &edges, unordered_map<int, list<int>> &adjList) {
+    for(int i = 0; i<edges.size(); i++) {
+        int u = edges[i][0];
+        int v = edges[i][1];
+
+        // Undirected
+        adjList[u].push_back(v);
+        adjList[v].push_back(u);
+    }
+}
+
+void dfs(int node, unordered_map<int, list<int>> &adjList, unordered_map<int, bool> &visited, vector<int> &temp) {
+    if(visited[node]) {
+        return;
+    }
+
+    visited[node] = true;
+    temp.push_back(node);
+
+    for(auto neighbor : adjList[node]) {
+        dfs(neighbor, adjList, visited, temp);
+    }
+
+}
+
+vector<vector<int>> depthFirstSearch(int V, int E, vector<vector<int>> &edges)
+{
+    // Write your code here
+    vector<vector<int>> ans;
+    unordered_map<int, list<int>> adjList;
+    unordered_map<int, bool> visited;
+
+    createAdjList(edges, adjList);
+
+    // Disconnected
+    for(int i=0; i<V; i++) {
+        if(!visited[i]) {
+            vector<int> temp;
+            dfs(i, adjList, visited, temp);
+            ans.push_back(temp);
+        }
+    }
+    return ans;
+}
+
 int main()
 {
 
